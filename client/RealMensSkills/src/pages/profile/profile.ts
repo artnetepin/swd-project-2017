224c37bce0444474acdfc34efa44ede62814ca0d
Artem Netepin <artnetepin@gmail.com>
2017-04-11 12:18:49 +0300

    Profile model updated

--- client/RealMensSkills/src/pages/profile/profile.ts
+++ client/RealMensSkills/src/pages/profile/profile.ts
@@ -7,8 +7,13 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'profile.html'
 })
 export class Profile {
+  skillCount = 12 as number;
+  achievementCount = 15 as number;
+  proofCount = 1 as number;
 
   constructor(public navCtrl: NavController) {
+
+
 
   }
 }
